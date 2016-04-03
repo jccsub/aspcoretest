00933f10fbd331f0ec8b333171a328547e628dd0
jccsub <chad.chandler@outlook.com>
2016-04-02 22:25:04 -0400

    Initial check in

---
+++ src/AspNetCoreTest/gulpfile.js
@@ -0,0 +1,16 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require("gulp"),
+	concat = require("gulp-concat"),
+	cssmin = require("gulp-cssmin"),
+	uglify = require("gulp-uglify");
+
+gulp.task("default", function () {
+	// place code for your default task here
+});
+
+
+loff https://app.pluralsight.com/player?course=asp-dot-net-5-understanding&author=roland-guijt&name=asp-dot-net-5-understanding-m1&clip=3&mode=live (5:01)
